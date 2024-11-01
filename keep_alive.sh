1baad65ea9fb5cff4d13e479c3e7b3b01aa21434
Matija Aleksić <matijaaleksic22@gmail.com>
2024-11-01 17:10:14 +0100

    modified keep_alive.sh

--- keep_alive.sh
+++ keep_alive.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-while true; do
-    if [ "$(docker ps -q -f name=time_api_app)" ]; then
-        echo "Container is running"
-    else
-        echo "Container is not running, restarting..."
-        docker start time_api_app
-    fi
-    sleep 60
-done
+docker update --restart always Time_Api_App
+
+echo "Restart policy set to 'always' for Time_Api_App container."
